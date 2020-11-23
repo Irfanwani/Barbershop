f9f8c458d94c4997d5c7e7e2931050ed528eddd1
Irfan wani <irfanwani347@gmail.com>
2020-11-23 21:03:51 +0530

    added service worker

---
+++ static/app.js
@@ -0,0 +1,5 @@
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register("{% static 'sw.js' %}")
+    .then(reg => console.log(reg, 'service worker registered.'))
+    .catch(error => console.log(error, "service worker not registered."));
+}
