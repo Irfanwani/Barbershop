cdfab107d556fbc2cbdcd1eb73f256703500ad22
Irfan wani <irfanwani347@gmail.com>
2020-11-25 18:30:07 +0530

    added fetch event in SW, theme color meta tag in base.html

--- barberapp/templates/barberapp/sw.js
+++ barberapp/templates/barberapp/sw.js
@@ -1,7 +1,14 @@
+// installation event
 self.addEventListener('install', evt => {
     console.log('Service worker has been installed.');
 });
 
+// activation event
 self.addEventListener('activate', evt => {
     console.log('Service worker has been activated.');
 }); 
+
+// fetch event
+self.addEventListener('fetch', evt => {
+    console.log('fetch event', evt);
+});
