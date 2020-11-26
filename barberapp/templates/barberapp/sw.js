ec05defc22892b06309a6fae2cf9191e13db5d25
Irfan wani <irfanwani347@gmail.com>
2020-11-26 19:01:39 +0530

    added caching

--- barberapp/templates/barberapp/sw.js
+++ barberapp/templates/barberapp/sw.js
@@ -1,14 +1,6 @@
 const staticCacheName = 'site-static';
 const assets = [
-    '/',
-    '/static',
-    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
-    'https://pro.fontawesome.com/releases/v5.10.0/css/all.css',
-    'https://code.jquery.com/jquery-3.5.1.min.js',
-    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.23.0/moment.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/css/tempusdominus-bootstrap-4.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/js/tempusdominus-bootstrap-4.min.js',
+    '/'
 ];
 
 // installation event
