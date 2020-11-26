6a2517de638bd6717db1eaecf7f72de66a9311d0
Irfan wani <irfanwani347@gmail.com>
2020-11-26 18:28:31 +0530

    added caching

--- barberapp/templates/barberapp/sw.js
+++ barberapp/templates/barberapp/sw.js
@@ -1,14 +1,33 @@
+const staticCacheName = 'site-static';
+const assets = [
+    '/',
+    '/static',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+    'https://pro.fontawesome.com/releases/v5.10.0/css/all.css',
+    'https://code.jquery.com/jquery-3.5.1.min.js',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.23.0/moment.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/css/tempusdominus-bootstrap-4.min.css',
+    '"https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/js/tempusdominus-bootstrap-4.min.js',
+]
+
 // installation event
 self.addEventListener('install', evt => {
-    console.log('Service worker has been installed.');
+    //console.log('Service worker has been installed.');
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+            console.log('caching assets');
+            cache.addAll(assets);
+        })
+    )
 });
 
 // activation event
 self.addEventListener('activate', evt => {
-    console.log('Service worker has been activated.');
+    //console.log('Service worker has been activated.');
 }); 
 
 // fetch event
 self.addEventListener('fetch', evt => {
-    console.log('fetch event', evt);
+    //console.log('fetch event', evt);
 });
