909b0c9c395f25ce1a436cf0dc920e6117138196
Irfan wani <irfanwani347@gmail.com>
2020-11-29 18:28:22 +0530

    caching assets

--- barberapp/templates/barberapp/sw.js
+++ barberapp/templates/barberapp/sw.js
@@ -1,6 +1,15 @@
 const staticCacheName = 'site-static';
 const assets = [
-    '/'
+    '/',
+    '/static/images/head_icon.png',
+    '/static/images/icon.png',
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css",
+    "https://pro.fontawesome.com/releases/v5.10.0/css/all.css",
+    "https://code.jquery.com/jquery-3.5.1.min.js",
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.23.0/moment.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/css/tempusdominus-bootstrap-4.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/tempusdominus-bootstrap-4/5.1.2/js/tempusdominus-bootstrap-4.min.js"
 ];
 
 // installation event
@@ -24,7 +33,7 @@ self.addEventListener('fetch', evt => {
     //console.log('fetch event', evt);
     evt.respondWith(
         caches.match(evt.request).then(casheRes => {
-            return casheRes
+            return casheRes;
         })
     )
 });
